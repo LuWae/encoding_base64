573a9f2020fedce32e126ec2906d7409566aee51
LuWae <l.waeldele@t-online.de>
2020-10-27 11:08:58 +0100

    decoding works

--- base64.c
+++ base64.c
@@ -32,10 +32,10 @@ static BYTE decode(char c) {
     return 0x1A + c - 'a';
 }
 
-#define WRITE(byte, buf, bp) (*((buf) + (bp)++) = encode(byte))
+#define WRITE_ENCODE(byte, buf, bp) (*((buf) + (bp)++) = encode(byte))
 
-int b64_encode(void *p, int length, char *buf) {
-    BYTE *cp = p;
+int b64_encode(const void *p, int length, char *buf) {
+    const BYTE *cp = p;
     BYTE a, b, c;
     int bp = 0;
     
@@ -47,62 +47,83 @@ int b64_encode(void *p, int length, char *buf) {
         b = *(cp + i + 1);
         c = *(cp + i + 2);
         
-        WRITE((a>>2) & 0x3F, buf, bp);
-        WRITE(((a<<4) & 0x30) + ((b>>4) & 0x0F), buf, bp);
-        WRITE(((b<<2) & 0x3C) + ((c>>6) & 0x03), buf, bp);
-        WRITE(c & 0x3F, buf, bp);
+        WRITE_ENCODE((a>>2) & 0x3F, buf, bp);
+        WRITE_ENCODE(((a<<4) & 0x30) |  ((b>>4) & 0x0F), buf, bp);
+        WRITE_ENCODE(((b<<2) & 0x3C) | ((c>>6) & 0x03), buf, bp);
+        WRITE_ENCODE(c & 0x3F, buf, bp);
     }
     
+    //end
     switch(length - i) {
         case 0:
             return i*4;
         case 1:
             a = *(cp + i);
-            WRITE((a>>2) & 0x3F, buf, bp);
-            WRITE((a<<4) & 0x30, buf, bp);
+            WRITE_ENCODE((a>>2) & 0x3F, buf, bp);
+            WRITE_ENCODE((a<<4) & 0x30, buf, bp);
             return i*4 + 2;
         case 2:
             a = *(cp + i);
             b = *(cp + i + 1);
-            WRITE((a>>2) & 0x3F, buf, bp);
-            WRITE(((a<<4) & 0x30) + ((b>>4) & 0x0F), buf, bp);
-            WRITE((b<<2) & 0x3C, buf, bp);
+            WRITE_ENCODE((a>>2) & 0x3F, buf, bp);
+            WRITE_ENCODE(((a<<4) & 0x30) + ((b>>4) & 0x0F), buf, bp);
+            WRITE_ENCODE((b<<2) & 0x3C, buf, bp);
             return i*4 + 3;
         default:
             return ERROR;
     }
 }
 
-int b64_decode(char *buf, int length, void *p) {
-    /*TODO*/
-    return 0;
-    
-    /*
+int b64_decode(const char *buf, int length, void *p) {
+    BYTE *cp = p;
     char a, b, c, d;
     int index = 0;
+    int i;
     
-    for(int i = 0; i < length - 3; i += 4) {
+    for(i = 0; i < length - 3; i += 4) {
+        //regular cases
         a = *(buf + i);
         b = *(buf + i + 1);
         c = *(buf + i + 2);
         d = *(buf + i + 3);
+        
+        *(cp + index) = ((decode(a)<<2) & 0xFC) | ((decode(b)>>4) & 0x03);
+        *(cp + index + 1) = ((decode(b)<<4) & 0xF0) | ((decode(c)>>2) & 0x0F);
+        *(cp + index + 2) = ((decode(c)<<6) & 0xC0) | ((decode(d)) & 0x3F);
+        
+        index += 3;
     }
-    */
+    
+    //end
+    switch(length - i) {
+        case 0:
+            return index;
+        case 2:
+            a = *(buf + i);
+            b = *(buf + i + 1);
+            *(cp + index) = ((decode(a)<<2) & 0xFC) | ((decode(b)>>4) & 0x03);
+            return index + 1;
+        case 3:
+            a = *(buf + i);
+            b = *(buf + i + 1);
+            c = *(buf + i + 2);
+            *(cp + index) = ((decode(a)<<2) & 0xFC) | ((decode(b)>>4) & 0x03);
+            *(cp + index + 1) = ((decode(b)<<4) & 0xF0) | ((decode(c)>>2) & 0x0F);
+            return index + 2;
+        default:
+            return ERROR;
+    }
 }
 
 int main() {
-    BYTE s[] = {0xFF, 0x00, 0xFF, 0x00, 0xAF, 0x70, 0xCC, 0x03};
+    char s[] = {'a', 'w'};
+    BYTE *p = malloc(100);
     
-    char *res = malloc(100);
-    int nbytes = b64_encode(s, 8, res);
-    if(nbytes >= 0) {
-        *(res + nbytes) = '\0';
-        printf("%s\n", res);
-    } else {
-        printf("encoding error\n");
+    int num_written = b64_decode(s, 2, p);
+    for(int i = 0; i < num_written; i++) {
+        printf("%d: %x\n", i, *(p + i));
     }
     
-    
-    free(res);
+    free(p);
     return 0;
 }
--- base64.h
+++ base64.h
@@ -1,7 +1,7 @@
 #ifndef BASE64_H
 #define BASE64_H
 
-int b64_encode(void *p, int length, char *buf);
-int b64_decode(char *buf, int length, void *p);
+int b64_encode(const void *p, int length, char *buf);
+int b64_decode(const char *buf, int length, void *p);
 
 #endif
